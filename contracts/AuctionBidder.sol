750aa27967cd6bb0df904687db62319a0d1581ca
Frank Maseo <leodubois@live.fr>
2023-02-03 15:01:27 +0100

    Added sushi pool price feed - undeployed

--- contracts/AuctionBidder.sol
+++ contracts/AuctionBidder.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GNU General Public License v3.0
+
 pragma solidity ^0.8.12;
 
 //import fluid auction interface
@@ -7,15 +8,25 @@ import "fluiddao/contracts/AuctionHouse.sol";
 
 //ERC721 receiver
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract AuctionBidder {
 	
 	address immutable owner;
 	AuctionHouse immutable auctionHouse;
+	address immutable WETH2;
+	address immutable FLUID;
+	address immutable sushiPool;
 
-	constructor(address auctionHouseAddress) {
+	///goerli sushiSwap pair address 0xd33c0bf246902ff4C87FA52C32F01aB0126Fda15
+	constructor(
+		address auctionHouseAddress
+	) {
 		owner = msg.sender;
 		auctionHouse = AuctionHouse(auctionHouseAddress);
+		sushiPool = address(0xd33c0bf246902ff4C87FA52C32F01aB0126Fda15);
+		WETH2 = address(0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6);
+		FLUID = address(0xBa01a0552C37036168b120165599193042c93B0E);
 	}
 
 	function onERC721Received(address, address, uint256, bytes calldata) external pure returns (bytes4) {
@@ -40,8 +51,15 @@ contract AuctionBidder {
 	
 
 	//returns the price in ETH of a FLUID token
-	function getFluidPrice() public view returns (uint256 price){
-		price = 2 * 10 ** 15 ; //hardcoded to 0.002 ETH
+	function getSushiPoolBalances() 
+	public 
+	view 
+	returns (
+		uint256 currentSushiReservesFluid,
+		uint256 currentSushiReservesWeth
+	){
+		currentSushiReservesFluid = IERC20(FLUID).balanceOf(sushiPool);
+		currentSushiReservesWeth = IERC20(WETH2).balanceOf(sushiPool);
 	}
 
 	function getFluidClaim() public view returns (
@@ -90,9 +108,12 @@ contract AuctionBidder {
 	function bidFloor(uint256 FLUIDnftId, uint256 currentBid) private{
 		//compute bid floor price
 		uint256 fluidClaim = getFluidClaim();
-		uint256 currentFluidPrice = getFluidPrice();
-		uint256 bidValue = currentFluidPrice * fluidClaim;
-		//place bid
+		(
+			uint256 currentSushiReservesFluid,
+			uint256 currentSushiReservesWeth
+		) = getSushiPoolBalances();
+
+		uint256 bidValue = currentSushiReservesFluid * fluidClaim / currentSushiReservesWeth;
 
 		//need to check if (1) possible to bid lower than current highest and (2) possible to remove bid
 		//To prevent the program from not bidding because current bid is higher -> current bid gets removed -> malicious user profits
